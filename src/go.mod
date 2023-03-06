618df138a0c1c8dbd6e714fd32a34a8f1f2bf293
Oleksandr Vasylets <alex.vasilets13@gmail.com>
2023-03-06 23:35:08 +0200

    Use Selenium to scrape the price of a game on Epic Games Store

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,7 @@
 module web-scraper
 
 go 1.20
+
+require github.com/tebeka/selenium v0.9.9
+
+require github.com/blang/semver v3.5.1+incompatible // indirect
--- src/main.go
+++ src/main.go
@@ -1,27 +1,80 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
-	"os"
-	"web-scraper/epicGames"
+	"time"
+
+	"github.com/tebeka/selenium"
+	"github.com/tebeka/selenium/chrome"
+	"github.com/tebeka/selenium/log"
 )
 
 func main() {
-	fmt.Print("Enter the name of the game: ")
-	var name string
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	if scanner.Err() != nil {
-		fmt.Fprintln(os.Stderr, scanner.Err().Error())
-		return
-	}
-	name = scanner.Text()
+	// fmt.Print("Enter the name of the game: ")
+	// var name string
+	// scanner := bufio.NewScanner(os.Stdin)
+	// scanner.Scan()
+	// if scanner.Err() != nil {
+	// 	fmt.Fprintln(os.Stderr, scanner.Err().Error())
+	// 	return
+	// }
+	// name = scanner.Text()
 
-	price, err := epicGames.GetPrice(name)
+	// price, err := epicGames.GetPrice(name)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+	// fmt.Println(price)
+
+	service, err := selenium.NewChromeDriverService("..\\bin\\chromedriver.exe", 4444)
 	if err != nil {
-		fmt.Println(err.Error())
-		return
+		panic(err)
 	}
-	fmt.Println(price)
+	defer service.Stop()
+
+	caps := selenium.Capabilities{}
+	caps.AddChrome(chrome.Capabilities{Args: []string{
+		"--no-sandbox",
+		"--disable-gpu",
+		"--headless",
+		"--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+		"--log-level=3",
+	}})
+	caps.SetLogLevel(log.Driver, log.Off)
+	caps.SetLogLevel(log.Browser, log.Off)
+
+	driver, err := selenium.NewRemote(caps, "")
+	if err != nil {
+		panic(err)
+	}
+	defer driver.Quit()
+
+	driver.SetImplicitWaitTimeout(100 * time.Millisecond)
+	driver.Get("https://store.epicgames.com/en-US/p/assassins-creed-1")
+	div, err := driver.FindElement(selenium.ByID, "_schemaOrgMarkup-Product")
+	if err != nil {
+		panic(err)
+	}
+	script, err := div.GetAttribute("innerHTML")
+	if err != nil {
+		panic(err)
+	}
+
+	type Data struct {
+		Offers []struct {
+			PriceSpecification struct {
+				Price         uint
+				PriceCurrency string
+			}
+		}
+	}
+
+	var data Data
+	err = json.Unmarshal([]byte(script), &data)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%d%s", data.Offers[0].PriceSpecification.Price, data.Offers[0].PriceSpecification.PriceCurrency)
 }
